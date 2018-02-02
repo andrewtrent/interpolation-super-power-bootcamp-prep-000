fb7aa202fbe28788da83778865e3aaa8c60bc0e4
Andrew Trent <andrewtrentdesign@gmail.com>
2018-02-02 19:02:38 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,10 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
   col_str = []
-  colors.each {|col| col_str.push("#{col[0].upcase}: #{col}")} 
-  col_str.each {|str| print str}
+  fin_str = ""
+  colors.each {|col| col_str.push("#{col[0].upcase}: #{col}") col_str.push(", ")} 
+  col_str.pop
+  col_str.each {|str| fin_str.push(str)}
+  print fin_str
 end
 display_rainbow(["red", "white", "blue"])
