762c2901870705d9dec5bb123b5bebc736db0ae4
Andrew Trent <andrewtrentdesign@gmail.com>
2018-02-02 19:03:27 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,7 +2,7 @@
 def display_rainbow(colors)
   col_str = []
   fin_str = ""
-  colors.each {|col| col_str.push("#{col[0].upcase}: #{col}") col_str.push(", ")} 
+  colors.each {|col| col_str.push("#{col[0].upcase}: #{col}",", ")} 
   col_str.pop
   col_str.each {|str| fin_str.push(str)}
   print fin_str
