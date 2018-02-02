49f661c799e92a40ddbd8baba18a0144b1301bb6
Andrew Trent <andrewtrentdesign@gmail.com>
2018-02-02 18:41:28 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
-  comma_colors = colors#
+  comma_colors = colors[0..-2]
   comma_colors.each {|col| print "#{col[0].upcase}: #{col}, "}
   print colors[-1]
 end
